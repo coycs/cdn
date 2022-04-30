a48db212b0ab98ebf7a1cd89dd86a81cef04cfd5
coycs <coycs@qq.com>
2022-04-30 11:37:11 +0800

    Hypothesis自定义标注颜色和透明度 0.2.0版本

--- tampermonkey-scripts/Hypothesis自定义标注颜色.js
+++ tampermonkey-scripts/Hypothesis自定义标注颜色.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hypothesis自定义标注颜色和透明度
 // @namespace    https://coycs.com/
-// @version      0.1.0
+// @version      0.2.0
 // @description  自定义标注颜色和透明度
 // @author       coycs
 // @match        http://*/*
@@ -14,12 +14,12 @@
 
 (function () {
   "use strict";
-  // webhook测试
-  print(1);
+
   window.onload = function () {
-    let color, transparency;
+    let color, transparency, font_color;
     color = getValue("H_color");
     transparency = getValue("H_transparency");
+    font_color = getValue("H_font_color");
     // 设置样式
     setStyle();
     // 注册脚本菜单
@@ -27,13 +27,16 @@
     // 弹出对话框
     function prompts() {
       // 获取自定义值
-      let p_color = prompt("请输入RGB颜色值（用英文逗号分隔）", color);
-      let p_transparency = prompt("请输入颜色透明度", transparency);
+      let p_color = prompt("请输入背景色RGB值（用英文逗号分隔）", color);
+      let p_transparency = prompt("请输入背景色透明度", transparency);
+      let p_font_color = prompt("请输入字体颜色", font_color);
       color=!p_color?color:p_color;
       transparency=!p_transparency?transparency:p_transparency;
+      font_color=!p_font_color?font_color:p_font_color;
       // 保存自定义值
       GM_setValue("H_color", color);
       GM_setValue("H_transparency", transparency);
+      GM_setValue("H_font_color", font_color);
       setStyle();
     }
     function setStyle() {
@@ -41,6 +44,7 @@
       style.type = "text/css";
       style.innerHTML = `.hypothesis-highlights-always-on .hypothesis-highlight {
       background-color: rgba(${color},${transparency}) !important;
+      color: ${font_color} !important;
     }`;
       document.getElementsByTagName("head").item(0).appendChild(style);
     }
@@ -53,6 +57,11 @@
         return (transparency = !GM_getValue("H_transparency")
           ? 0.4
           : GM_getValue("H_transparency"));
+      }
+      if (ag == "H_font_color") {
+        return (font_color = !GM_getValue("H_font_color")
+          ? "#000"
+          : GM_getValue("H_font_color"));
       }
     }
   };
